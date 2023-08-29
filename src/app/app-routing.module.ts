58c007b273545938c74fc814cdcae213eed1d66a
BennyDRG <bennyboy260@gmail.com>
2023-08-29 23:15:04 +0200

    Ajout formulaire

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'pokemons', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent } // A mettre en dernier sinon interese^tera toutes les routes!
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
